a8fba8cb3df06f6ac05808db1cb8ce2b7be5885d
Henadzi_Misuna <henadzi30@gmail.com>
2023-02-21 12:42:11 +0300

    added a methods of functions & while, for metods

---
+++ Example012_Function/Program.cs
@@ -0,0 +1,170 @@
+﻿// Метод 2 - принимает Именнованные аргументы
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Method21("Текст", 4);
+// Method21(msg: "Такая программа", count: 4);
+Method21(count: 4, msg: "Такая программа");
+
+// Метод 3 - аргументы не передаются, а значение возвращается
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Метод 4 - что-то принимает и что-то возвращает
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "Go");
+Console.WriteLine(res);
+
+// или
+
+string Method4_2(int count_2, string text_2)
+{
+    string result_2 = String.Empty;
+
+    for (int i = 0; i < count_2; i++)
+    {
+        result_2 = result_2 + text_2;
+    }
+    return result_2;
+}
+string res_2 = Method4_2(10, "foo");
+Console.WriteLine(res_2);
+
+// Таблица умножения 
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+// Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+// заменить большими «К», а большие «С» заменить на маленькие «с».
+
+string text_3 = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result_3 = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result_3 = result_3 + $"{newValue}";
+        else result_3 = result_3 + $"{text[i]}";
+    }
+
+    return result_3;
+}
+string newText = Replace(text_3, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
+// Алгоритм сортировки методом выбора по возрастанию
+
+int[] arr = {1, 5, 8, 4, 3, 6, 2, 7, 3, 9, 2, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+
+// Алгоритм сортировки методом выбора по убыванию
+
+int[] arr = {1, 5, 8, 4, 3, 6, 2, 7, 3, 9, 2, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
